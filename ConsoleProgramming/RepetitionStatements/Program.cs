0d0528bcb5861454fdefbfe72e227b720f8f894c
Barzin <barzin.development@gmail.com>
2022-11-04 00:48:37 +0100

    Add addTwoNumbers program as practice for learning looping (repetition statemets) in C#. Also involved (my own research on how to) recursion so the program can call it self and provide the questions. Als practiced try/catch method for exeption handling.

---
+++ ConsoleProgramming/RepetitionStatements/Program.cs
@@ -0,0 +1,60 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+bool proceed = true;
+
+bool getUserChoise()
+{
+    
+    try
+    {
+        Console.Write($"Would you like to proceed? (y or n) => ");
+        char userChoise = Convert.ToChar(Console.ReadLine());
+        if (userChoise == 'y' || userChoise == 'n')
+        {
+            proceed = (userChoise == 'y') ? true : false;
+        }
+    }
+    catch (Exception ex)
+    {
+        Console.WriteLine($"\nYou're input was invalid! {ex.Message}\n\nTry again, enter only 'y' or 'n' !!!");
+        getUserChoise();
+    }
+
+   return proceed;
+}
+
+
+
+void addTwoNumbers()
+{
+    try
+    {
+        
+        Console.WriteLine($"Enter your first number: ");
+        double num1 = Convert.ToDouble(Console.ReadLine());
+
+        Console.WriteLine($"Enter your second number: ");
+        double num2 = Convert.ToDouble(Console.ReadLine());
+
+        double result = num1 + num2;
+        Console.WriteLine($"Result of {num1} + {num2} = {result}");
+        Console.WriteLine($"\n=============================================================\n");
+        proceed = getUserChoise();
+
+    }
+    catch (Exception ex)
+    {
+        Console.WriteLine($"\nYou're input was invalid! {ex.Message}\n\nTry again!");
+        addTwoNumbers();
+    }
+}
+
+
+do
+{
+    Console.WriteLine("This application adds the two numbers you provide.");
+    addTwoNumbers();
+   
+} while (proceed);
+
+
